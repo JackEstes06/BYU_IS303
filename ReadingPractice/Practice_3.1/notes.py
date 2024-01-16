81bf14b98c728e2b179691270856948c3f04d1c1
jackestes06 <jackestes3210@gmail.com>
2024-01-16 09:20:28 -0700

    Chapter 3 stuffs

---
+++ ReadingPractice/Practice_3.1/notes.py
@@ -0,0 +1,29 @@
+# Author: Jack Estes
+# BYU IS303
+# 1/16/24
+
+# first var - should be more descriptive
+# named vars that are long should be named_with_snake_case
+x = 10
+
+# types of vars
+# strings
+first_name = "Mickey"
+first_name = 'Mickey'
+# ints
+my_age = 24
+num_1 = 0
+num_2 = 0
+num_2 = 5
+# flaots
+my_gpa = 2.78  # prints 2.78
+my_gpa = 3.00  # prints 3.0
+# booleans
+am_hungry = True
+am_hangry = False
+# Binary
+ascii_character = bytes(1)  # variable would be 1 byte long but currently does not have a value
+ascii_character = int('01000001', 2)  # value of the variable is one byte of 01000001 ASCI value 65.
+print(ascii_character)  # this would display 65
+print(chr(ascii_character))  # this would display A.
+# Many others utilized - used type(var) to figure out var type
---
+++ ReadingPractice/Practice_3.1/pracitce.py
@@ -0,0 +1,23 @@
+# Description: This program adds two numbers and prints the sum
+
+"""
+Ingredients:
+  Hold first value
+  Hold second value
+  Hold sum of first and second
+
+Directions:
+   Create the first holding area and assign it a value
+   Create the second holding area and assign it a value
+   Create the sum holding area and assign it the sum of first and second values
+   Display the contents of the sum holding area
+"""
+
+# Author: Jack Estes
+# BYU IS303
+# 1/16/24
+
+first_num = 37
+second_num = 4907283
+sum = first_num + second_num
+print("The sum is: ", sum)
