9856f8a30c1b59f02498b24338af682b406943db
jackestes06 <jackestes3210@gmail.com>
2024-01-23 11:11:30 -0700

    HW A2 completed

--- hw/A2-BMI.py
+++ hw/A2-BMI.py
@@ -3,10 +3,11 @@
 # BMI Calculator
 
 # Instantiate vars
-fHeightInFeet = 0
+iHeightInFeet = 0
 fHeightInInches = 0
 fWeightInPounds = 0
 fBMIVal = 0
+sBMIType = "Normal Weight"
 
 # Get user's name & body info
 sFName = input("Enter your first name: ")
@@ -14,15 +15,45 @@ sLName = input("Enter your first name: ")
 # Verify that the user input can be converted to an numbers
 while True:
     try:
-        iHeightInFeet = int(input("Enter your height in feet: "))
-        break
-    except ValueError:
-        print("You entered a non-integer value, try again.")
-        continue
-while True:
-    try:
-        iHeightInInches = float(input("Enter your height in inches: "))
+        iHeightInFeet = int(input("Enter the feet portion of your height: "))
         break
     except ValueError:
         print("You entered a non-numeric value, try again.")
         continue
+while True:
+    try:
+        fHeightInInches = float(input("Enter the inches portion of your height: "))
+        break
+    except ValueError:
+        print("You entered a non-numeric value, try again.")
+        continue
+while True:
+    try:
+        fWeightInPounds = float(input("Enter your weight in pounds: "))
+        break
+    except ValueError:
+        print("You entered a non-numeric value, try again.")
+        continue
+
+# Calculate BMI & output to user
+# Convert height to total inches
+fHeightInInches += (iHeightInFeet*12)
+# There are two ways to calculate BMI, one of the below lines is commented out since we only need 1
+fBMIVal = fWeightInPounds/(fHeightInInches ** 2) * 703
+# fBMIVal = ((fWeightInPounds/fHeightInInches)/fHeightInInches) * 703
+# Determine BMI Type based on BMI Value
+if fBMIVal < 18.5:
+    sBMIType = "Underweight"
+elif 18.5 <= fBMIVal < 25:
+    sBMIType = "Normal Weight"
+elif 25 <= fBMIVal < 30:
+    sBMIType = "Overweight"
+else:
+    sBMIType = "Obese"
+
+print("{fName} {lName}\nhas a BMI of {bmiVal} and is {bmiType}".format(
+    fName=sFName,
+    lName=sLName,
+    bmiVal=round(fBMIVal,2),
+    bmiType=sBMIType
+))
