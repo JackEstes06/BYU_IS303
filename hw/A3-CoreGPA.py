dd6af226217b38b76422d964d7503c93bb4a208a
jackestes06 <jackestes3210@gmail.com>
2024-02-01 10:28:43 -0700

    Tweaks to HW3

--- hw/A3-CoreGPA.py
+++ hw/A3-CoreGPA.py
@@ -4,6 +4,15 @@
 # and gives the chances of getting into the IS program based on
 # what was given.
 
+# Takes input and ensures value is no less than fLow and no higher than fHigh
+def check_range(toCheck, low, high):
+    if toCheck > high:
+        toCheck = high
+    if toCheck < low:
+        toCheck = low
+    return toCheck
+
+
 # Mapped values of each letter grade and its value
 gpaValues = {
     "A": 4.0, "A-": 3.7,
@@ -20,22 +29,26 @@ fOverallGPAWeight = 0.12
 fLast30GPAWeight = 0.18
 fEssayWeight = 0.05
 
-# Get variable inputs from user
+# Get variable inputs from user & ensure values are within proper ranges
+# (i.e. GPA can't be greater than 4.0 or less than 0.0)
 sIS201LetterGrade = input("What letter grade did you get for IS201 (i.e. A-)? ").upper()
 sIS303LetterGrade = input("What letter grade did you get for IS303 (i.e. A-)? ").upper()
 sAcc200LetterGrade = input("What letter grade did you get for ACC200 (i.e. A-)? ").upper()
-fOverallGPA = float(input("What is your current overall GPA? "))
-fLast30GPA = float(input("What is the GPA from your most recent 30 credits? "))
-iEssayScore = int(input("What will you score on the IS application essays (whole numbers - scored 0 to 4)? "))
+# check_range will force each of these inputs to be no lower than param2 & no higher than param3
+# (i.e. fOverallGPA input is 46, it will be forced down to 4.0)
+fOverallGPA = check_range(float(input("What is your current overall GPA? ")), 0, 4)
+fLast30GPA = check_range(float(input("What is the GPA from your most recent 30 credits? ")), 0, 4)
+iEssayScore = check_range(
+    int(input("What will you score on the IS application essays (whole numbers - scored 0 to 4)? ")), 0, 4)
 
 # Calculate weighted application GPA & output to user
 fApplicationGPA = round(
-        (gpaValues.get(sAcc200LetterGrade) * fAcc200Weight)
-        + (gpaValues.get(sIS201LetterGrade) * fIS201Weight)
-        + (gpaValues.get(sIS303LetterGrade) * fIS303Weight)
-        + (fOverallGPA * fOverallGPAWeight)
-        + (fLast30GPA * fLast30GPAWeight)
-        + (iEssayScore * fEssayWeight), 3
+    (gpaValues.get(sAcc200LetterGrade) * fAcc200Weight)
+    + (gpaValues.get(sIS201LetterGrade) * fIS201Weight)
+    + (gpaValues.get(sIS303LetterGrade) * fIS303Weight)
+    + (fOverallGPA * fOverallGPAWeight)
+    + (fLast30GPA * fLast30GPAWeight)
+    + (iEssayScore * fEssayWeight), 3
 )
 print("Total Applying GPA is {gpa}".format(
     gpa=fApplicationGPA
