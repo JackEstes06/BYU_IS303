f6ee21ed2717a17eec78777b9f8caedd48e46558
jackestes06 <jackestes3210@gmail.com>
2024-03-19 12:37:31 -0600

    HW Stuffs

--- ClassPractice/March/dictionary_pt2.py
+++ ClassPractice/March/dictionary_pt2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# Create data personally
 dctCandy = {
     "Description": ["SNICKERS", "KIT KAT", "SKITTLES"],
     "Calories": [280, 218, 81],
@@ -7,12 +8,18 @@ dctCandy = {
 }
 
 dfCandy = pd.DataFrame(dctCandy)
-
 dfCandy = dfCandy.sort_values(
     by=['Fat_Content', 'Description'],
     ascending=[True, True],
 )
-
 print(f'{dfCandy}\n')
 print(f'{dfCandy["Description"]}\n')
-print(f'{dfCandy[["Description", "Fat_Content"]]}\n')
+print(f'{dfCandy[["Description", "Fat_Content"]]}\n\n\n')
+
+
+# Extract data from spreadsheet
+dfTeams = pd.read_csv(
+    "pandasPractice.csv",
+    index_col="Team",
+)
+print(dfTeams)
