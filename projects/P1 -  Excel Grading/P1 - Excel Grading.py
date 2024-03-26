5ee69f760eb04eef7fefc6b639127648a84474d0
jackestes06 <jackestes3210@gmail.com>
2024-03-26 10:12:52 -0600

    Python stuff

---
+++ projects/P1 -  Excel Grading/P1 - Excel Grading.py
@@ -0,0 +1,9 @@
+# Jack Estes, Katelyn Hamilton, Hannah Larkin, Sebastian Mcfarland, Allen Schultz, Hayden Whalen
+# IS303 Section 3
+#
+# Project 1 - Take poorly formatted input data from excel sheets and create a more organized excel sheet w/
+# different workbooks & statistics
+
+from openpyxl import Workbook
+
+
