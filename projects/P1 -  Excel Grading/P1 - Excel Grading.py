d6e7b40908747abcd0ab950d05f5885f6d51f00d
jackestes06 <jackestes3210@gmail.com>
2024-03-28 09:11:27 -0600

    More functionality & comments to group Project

--- projects/P1 -  Excel Grading/P1 - Excel Grading.py
+++ projects/P1 -  Excel Grading/P1 - Excel Grading.py
@@ -8,6 +8,7 @@ from openpyxl import Workbook
 from openpyxl.styles import Font
 
 
+# Create each sheet's structure within the workbook
 def startPage(name, myBook):
     # Headers
     currSheet = myBook[name]
@@ -37,27 +38,32 @@ def startPage(name, myBook):
     currSheet.cell(6, 6).value = "Number of Students"
 
 
+# Create a sheet for each unique class in the poorly organized data in an excel file
 def createSheets(poorData, classesList, goodData):
     for row in poorData:
+        # Ignore headers in poor data file
         if row[0].value == "Class Name":
             pass
+        # Add the class name to the classesList if it isn't already in there
         elif not classesList.__contains__(row[0].value):
             print(row[0].value)
             classesList.append(row[0].value)
             goodData.create_sheet(row[0].value)
             startPage(row[0].value, goodData)
-            setGrades(goodData[row[0].value])
-    # TODO figure out where to put this
-    # Parse the data for each sheet
-    parseData(poorData, classesList, goodData)
-    # end TODO
 
 
-def parseData(poorData, classesList, goodData):
-    print("Parse data called")
-    # TODO parse data
+# Organizes the data into each respective class
+def parseData(poorData, goodData):
+    classesList = goodData.sheetnames
+    print(f'Classes: {classesList}')
+    # TODO parse data into respective sheets
+    for row in poorData:
+        print(row[0].value)
+        print(row[1].value)
+        print(row[2].value)
 
 
+# Sets class statistics within each sheet of the workbook
 def setGrades(currSheet):
     currSheet["G2"] = '=MAX(OFFSET($D$2,0,0,COUNT($D:$D)-1,1))'
     currSheet["G3"] = '=MIN(OFFSET($D$2,0,0,COUNT($D:$D)-1,1))'
@@ -81,6 +87,14 @@ createSheets(poorDataXlsx2.active, [], goodDataXlsx2)
 # Remove default sheets if they exist
 goodDataXlsx1.__delitem__("Sheet")
 goodDataXlsx2.__delitem__("Sheet")
+
+# Parse data into each sheet
+parseData(poorDataXlsx1.active, goodDataXlsx1)
+parseData(poorDataXlsx2.active, goodDataXlsx2)
+
+# Set the grade statistics values for each sheet
+for sheet in goodDataXlsx1.sheetnames:
+    setGrades(goodDataXlsx1[sheet])
 
 # Save the well organized data
 goodDataXlsx1.save(filename="Well_Organized_Data_1.xlsx")
